77f5850a0ee59daaf3ab288a9fcf1ca86cb1f1f6
AlexReal <alexander.d.mills@gmail.com>
2026-08-23 20:10:04 +0000

    chore(env): add sops/age/just to the nix dev shell

--- flake.nix
+++ flake.nix
@@ -3,5 +3,5 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   outputs = { self, nixpkgs }:
     let system = "x86_64-linux"; pkgs = import nixpkgs { inherit system; };
-    in { devShells.${system}.default = pkgs.mkShell { packages = with pkgs; [ rustup pkg-config openssl postgresql nodejs_22 ]; }; };
+    in { devShells.${system}.default = pkgs.mkShell { packages = with pkgs; [ sops age just rustup pkg-config openssl postgresql nodejs_22 ]; }; };
 }
